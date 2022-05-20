e8d6993734fa9fbe69ee962c0ee1ba5bafde551d
deepeters <dennis95peters@gmail.com>
2022-05-20 14:23:50 +0300

    fix minor issues and bugs and add screenshots

--- src/components/People.js
+++ src/components/People.js
@@ -16,77 +16,82 @@ const PEOPLE_QUERY = gql`
   query MyQuery {
     getPeople {
       results {
-        gender
-        height
-        homeworld
-        mass
-        name
         url
+        name
+        mass
+        homeworld
+        height
+        gender
       }
     }
   }
 `;
 
 function People() {
-  const { loading, error, data } = useQuery(PEOPLE_QUERY);
+  const { loading, error, data: peopleData } = useQuery(PEOPLE_QUERY);
 
   if (loading) return "Loading...";
-  if (error) return <pre>{error.message}</pre>;
+  if (error) return <pre className="look">{error.message}</pre>;
 
-  console.log(data);
-
-  return data.results.map(({ url, name, mass, homeworld, height, gender }) => (
-    <div key={name}>
-      <Card className="border border-dark bg-dark text-white">
-        <Card.Header>
-          <FontAwesomeIcon icon={faList} />
-          PeopleList
-        </Card.Header>
-        <Card.Body>
-          <Table striped bordered hover variant="dark">
-            <thead>
-              <tr>
-                {/* <th>#</th> */}
-                <th>Name</th>
-                <th>Mass</th>
-                <th>Height</th>
-                <th>Gender</th>
-                <th>Homeworld</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* {this.state.people.length === 0 ? (
+  console.log(peopleData);
+  return (
+    <>
+      <Card.Header className="text-white">
+        <FontAwesomeIcon icon={faList} />
+        <span> PeopleList</span>
+      </Card.Header>
+      {peopleData.getPeople.results.map(({ url, name, mass, homeworld, height, gender }) => (
+        <div key={name}>
+          <Card className="border border-dark bg-dark text-white">
+            <Card.Body>
+              <Table striped bordered hover variant="dark">
+                <thead>
+                  <tr>
+                    {/* <th>#</th> */}
+                    <th>Name</th>
+                    <th>Mass</th>
+                    <th>Height</th>
+                    <th>Gender</th>
+                    <th>Homeworld</th>
+                    <th>URL</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* {this.state.people.length === 0 ? (
                 <tr align="center">
                   <td colSpan="6"> No People Available</td>
                 </tr>
               ) : (
                 this.state.people.map((people) => ( */}
-              <tr key={url}>
-                <td>{name}</td>
-                <td>{mass}</td>
-                <td>{homeworld}</td>
-                <td>{height}</td>
-                <td>{gender}</td>
-                <td>
-                  <ButtonGroup>
-                    <Link to={`/person/$ {name} `} className="btn btn-sm btn-outline-primary">
-                      <FontAwesomeIcon icon={faEdit} />
-                    </Link>{" "}
-                    {/* <Button size="sm" variant="outline-danger" onClick={this.deleteProduct.bind(this, people.id)}>
+                  <tr key={name}>
+                    <td>{name}</td>
+                    <td>{mass}</td>
+                    <td>{height}</td>
+                    <td>{gender}</td>
+                    <td>{homeworld}</td>
+                    <td>{url}</td>
+                    <td>
+                      <ButtonGroup>
+                        <Link to={`/person/${name}`} className="btn btn-sm btn-outline-primary">
+                          <FontAwesomeIcon icon={faEdit} />
+                        </Link>{" "}
+                        {/* <Button size="sm" variant="outline-danger" onClick={this.deleteProduct.bind(this, people.id)}>
                           <FontAwesomeIcon icon={faTrash} />
                         </Button> */}
-                  </ButtonGroup>
-                </td>
-              </tr>
-              {/* ))
+                      </ButtonGroup>
+                    </td>
+                  </tr>
+                  {/* ))
               )} */}
-            </tbody>
-          </Table>
-        </Card.Body>
-      </Card>
-    </div>
-  ));
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default People;
